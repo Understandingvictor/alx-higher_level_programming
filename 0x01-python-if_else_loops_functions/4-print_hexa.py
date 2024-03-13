f52708173898614ae68f47441c03fe4d687c29c9
Understandingvictor <victorflawless7@gmailcom>
2024-03-13 21:59:29 +0100

    made it pycodestyle compliant

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 99):
-	if i < 10:
-		print("{} = 0x{}".format(i, i))
-	if i >= 10:
-		print("{} = 0x{:x}".format(i, i))
+    if i < 10:
+        print("{} = 0x{}".format(i, i))
+    if i >= 10:
+        print("{} = 0x{:x}".format(i, i))
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 def islower(c):
-	for i in range(97, 123):
-		character = chr(i)
-		if character == c:
-			return True
-	return False
+    for i in range(97, 123):
+        character = chr(i)
+        if character == c:
+            return True
+    return False
