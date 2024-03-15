43ebc0777c1b964011f74e93c212767b04e898e3
Understandingvictor <victorflawless7@gmailcom>
2024-03-15 21:29:40 +0100

    corrected indentation

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-import calculator_1
-import sys
-args = sys.argv[:]
+if __name__ == '__main__':
+    import calculator_1
+    import sys
+    args = sys.argv[:]
 
-if len(args) < 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>".format())
-    exit(1)
+    if len(args) < 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>".format())
+        exit(1)
 
-a = int(sys.argv[1])
-b = int(sys.argv[3])
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
 
-if sys.argv[2] == "+":
-    result = calculator_1.add(a, b)
-    print("{} + {} = {}".format(a, b, result))
-elif sys.argv[2] == "-":
-    result = calculator_1.sub(a, b)
-    print("{} - {} = {}".format(a, b, result))
-elif sys.argv[2] == "*":
-    result = calculator_1.mul(a, b)
-    print("{} * {} = {}".format(a, b, result))
-elif sys.argv[2] == "/":
-    result = calculator_1.div(a, b)
-    print("{} / {} = {}".format(a, b, result))
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
+    if sys.argv[2] == "+":
+        result = calculator_1.add(a, b)
+        print("{} + {} = {}".format(a, b, result))
+    elif sys.argv[2] == "-":
+        result = calculator_1.sub(a, b)
+        print("{} - {} = {}".format(a, b, result))
+    elif sys.argv[2] == "*":
+        result = calculator_1.mul(a, b)
+        print("{} * {} = {}".format(a, b, result))
+    elif sys.argv[2] == "/":
+        result = calculator_1.div(a, b)
+        print("{} / {} = {}".format(a, b, result))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
