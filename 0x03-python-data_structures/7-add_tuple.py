76398e5444cef27bfeec1d070f7309800acdcfd5
Understandingvictor <victorflawless7@gmailcom>
2024-03-18 19:07:05 +0100

    Added new files

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,39 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
+        result = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+        return result
+    if len(tuple_a) < 2 or len(tuple_b) < 2:
+        if len(tuple_a) == 1 and len(tuple_b) > 1:
+            result = ((tuple_a[0] + tuple_b[0]), (0 + tuple_b[1]))
+            return result
+        elif len(tuple_a) > 1 and len(tuple_b) == 1:
+            result = ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + 0))
+            return result
+        elif len(tuple_a) == 0 and len(tuple_b) > 1:
+            result = ((0 + tuple_b[0]), (0 + tuple_b[1]))
+            return result
+        elif len(tuple_a) > 1 and len(tuple_b) == 0:
+            result = ((tuple_a[0] + 0), (tuple_a[1] + 0))
+            return result
+        elif len(tuple_a) == 0 and len(tuple_b) == 1:
+            result = ((0 + tuple_b[0]), (0 + 0))
+            return result
+        elif len(tuple_a) == 1 and len(tuple_b) == 0:
+            result = ((tuple_a[0] + 0), (0 + 0))
+            return result
+        elif len(tuple_a) == 0 and len(tuple_b) == 0:
+            result = ((0), (0))
+            return result
+        else:
+            result = ((tuple_a[0] + tuple_b[0]), (0 + 0))
+            return result
+  
+
+
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,5 @@
+def multiple_returns(sentence):
+    _len = len(sentence)
+    Fcharacter = sentence[0]
+
+    return ((_len), (Fcharacter))
